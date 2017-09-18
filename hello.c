a20bfdf4fd65951ac63c7c09928a25190804088b
forhack <1004xlsrn@gmail.com>
2017-09-18 19:35:21 +0900

    strcmp 코드 구현

--- hello.c
+++ hello.c
@@ -17,5 +17,13 @@ char *myStrcat(char *dest, const char *src) {
 }
 
 int myStrcmp(const char *string1, const char *string2) {
-    return 0;
+    for (int i = 0; ; i++) {
+        if (string1[i] != string2[i]) {
+            return string1[i] < string2[i] ? -1 : 1;
+        }
+
+        if (string1[i] == '\0') {
+            return 0;
+        }
+    }
 }
