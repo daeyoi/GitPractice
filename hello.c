42a912b5c577bc8ab418040ce831531b911995cc
DESKTOP-17O5VQ1\daeyo <daeyoi96@gmail.com>
2017-09-18 19:26:24 +0900

    strcpy

--- hello.c
+++ hello.c
@@ -1,21 +1,33 @@
 #include "hello.h"
 
 int main() {
-    printf("hello world\n");
+	printf("hello world\n");
 }
 
 char *myStrcpy(char *dest, const char *src) {
-    return NULL;
+	
+	int i = 0;
+	while(1){
+
+		if(src[i]!=NULL){
+			dest[i]=src[i];
+			i++;
+		}else{
+			break;
+		}
+
+	}
+	return NULL;
 }
 
 size_t myStrlen(const char *string) {
-    return 0;
+	return 0;
 }
 
 char *myStrcat(char *dest, const char *src) {
-    return NULL;
+	return NULL;
 }
 
 int myStrcmp(const char *string1, const char *string2) {
-    return 0;
+	return 0;
 }
